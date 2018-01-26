48102a21058985fe74f996fde29f56e24b419df8
psyaoc <psyaoc@gmail.com>
2018-01-26 14:10:31 +0000

    changed ng-repeat to match json format

--- public/core.js
+++ public/core.js
@@ -3,19 +3,19 @@ var healthtrack = angular.module('healthtrack', []);
 function mainController($scope, $http) {
     $scope.formData = {};
 
-    // On controller load get movesPlaces
-    $http.get('/databox-app-healthtrack/ui/api/movesPlaces').then(function (success) {
-        console.log(success);
-        $scope.movesPlaces = success;
-    }, function (error) {
-        console.log('Error: ' + error);
-    });
-
     $scope.parseJson = function (json) {
     	let parsed = JSON.parse(json);
     	console.log("JSON Parsed: " + parsed);
     	return parsed;
     }
+
+    // On controller load get movesPlaces
+    $http.get('/databox-app-healthtrack/ui/api/movesPlaces').then(function (success) {
+        $scope.movesPlaces = JSON.parse(success);
+    }, function (error) {
+        console.log('Error: ' + error);
+    });
+    
 
 }
 
