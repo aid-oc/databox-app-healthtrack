97589eb49750fd9f19ef443c5620cd58d2e52857
psyaoc <psyaoc@gmail.com>
2018-01-26 14:00:00 +0000

    added parseJson function

--- public/core.js
+++ public/core.js
@@ -3,12 +3,17 @@ var healthtrack = angular.module('healthtrack', []);
 function mainController($scope, $http) {
     $scope.formData = {};
 
+    // On controller load get movesPlaces
     $http.get('/databox-app-healthtrack/ui/api/movesPlaces').then(function (success) {
         console.log(success);
         $scope.movesPlaces = success;
     }, function (error) {
         console.log('Error: ' + error);
     });
+
+    $scope.parseJson = function (json) {
+    	return JSON.parse(json);
+    }
 
 }
 
