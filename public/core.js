a801516840a9a6c05bd31c8adb866390a3620847
psyaoc <aoc_i@icloud.com>
2018-02-09 17:48:33 +0000

    Trying to find a cleaner way of handling imports

--- public/core.js
+++ public/core.js
@@ -6,6 +6,48 @@ function mainController($scope, $http, $window, $document, $mdDialog) {
 
     $scope.monthlyFeedbackGiven = 0;
 
+
+    const data = {
+        // Labels should be Date objects
+        labels: [new Date(2017, 08, 16), new Date(2017, 08, 17), new Date(2017, 08, 18)],
+        datasets: [{
+            fill: false,
+            label: 'Page Views',
+            data: [280, 250, 340],
+            borderColor: '#fe8b36',
+            backgroundColor: '#fe8b36',
+            lineTension: 0,
+        }]
+    }
+    const options = {
+        type: 'line',
+        data: data,
+        options: {
+            fill: false,
+            responsive: true,
+            scales: {
+                xAxes: [{
+                    type: 'time',
+                    display: true,
+                    scaleLabel: {
+                        display: true,
+                        labelString: "Date",
+                    }
+                }],
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true,
+                    },
+                    display: true,
+                    scaleLabel: {
+                        display: true,
+                        labelString: "Page Views",
+                    }
+                }]
+            }
+        }
+    }
+
     $scope.parseJson = function(json) {
         let parsed = JSON.parse(json);
         console.log("JSON Parsed: " + parsed);
